9d572c5308a0bfab1d37acc34fc5faba2e0ec204
zocodo <1175353458@qq.com>
2021-12-31 12:00:04 +0800

    红魔6设备树
    红魔6设备树

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_NX669J.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_NX669J-user \
+    omni_NX669J-userdebug \
+    omni_NX669J-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_NX669J-user
+add_lunch_combo omni_NX669J-userdebug
+add_lunch_combo omni_NX669J-eng
