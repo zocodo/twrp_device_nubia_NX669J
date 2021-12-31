69765c6484ebe978ef89d762ac6f4fe3c0aaa5aa
zocodo <1175353458@qq.com>
2021-12-31 14:58:23 +0800

    Update omni_NX669J.mk

--- omni_NX669J.mk
+++ omni_NX669J.mk
@@ -5,17 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from NX669J device
-$(call inherit-product, device/nubia/NX669J/device.mk)
-
-# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-# $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := NX669J
