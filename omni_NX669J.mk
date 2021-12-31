ebb45874d3b2d2c27574bda31b721e23d1203daf
zocodo <1175353458@qq.com>
2021-12-31 14:56:04 +0800

    Update omni_NX669J.mk

--- omni_NX669J.mk
+++ omni_NX669J.mk
@@ -14,12 +14,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/nubia/NX669J/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+# $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := NX669J
-PRODUCT_NAME := omni_NX669J
+PRODUCT_NAME := twrp_NX669J
 PRODUCT_BRAND := nubia
 PRODUCT_MODEL := NX669J
 PRODUCT_MANUFACTURER := nubia
